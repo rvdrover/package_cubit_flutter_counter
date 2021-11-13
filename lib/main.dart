43f1fbab8707bd96712fc7926290108fca23a027
rvdrover <53379496+rvdrover@users.noreply.github.com>
2021-11-14 01:47:36 +0530

    on air

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+
+import 'bloc_observer.dart';
+import 'cubit/counter_cubit.dart';
+import 'home.dart';
+void main() {
+  Bloc.observer = CounterObserver();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter State',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  BlocProvider(
+        create: (context) => CounterCubit(),
+        child: const MyHomePage(title: 'Flutter Package Cubit Counter'),
+      ),
+    );
+  }
+}
